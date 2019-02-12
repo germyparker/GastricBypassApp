1720e192c70fedb57ad180c43605a6520275517b
n blancard <nblancard@gmail.com>
2019-02-12 14:10:59 -0600

    adds timers and countdown, does not report properly

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -20,14 +20,19 @@ export default class HomeScreen extends React.Component {
   constructor(props) {
       super(props);
       this.state = { 
-          nap: 10,
-          food: 240,
-          bp: 360,
-          water: 480,
-          pulseox: 600
+          ok: 'ok',
+                nap: 10,
+                food: 2400,
+                bp: 3600,
+                water: 4800,
+                pulseox: 6000
       }
+      this.timer = this.timer.bind(this);
   }
 
+  componentDidMount() {
+    setInterval(this.timer, 1000);
+}
     render() {
         return (
             <View style={styles.main}>
@@ -39,32 +44,32 @@ export default class HomeScreen extends React.Component {
                     <Text>Here are the things you need to do:</Text>
                       <FlatList 
                       data={[
-                          {text: 'Nap Every 2 Hours', dur: 120, key: 'nap'},
-                          {text: 'Eat Food Every 4 Hours', dur: 240, key: 'food'},
-                          {text: 'Check BP every 6 Hours', dur: 360, key: 'bp'},
-                          {text: 'Drink 8oz Water every 8 Hours', dur: 480, key: 'water'},
-                          {text: 'Check Pulse O2 every 10 Hours', dur: 600, key: 'pulseox'},
+                          {text: 'Nap Every 2 Hours', dur: 1200, key: 'nap'},
+                          {text: 'Eat Food Every 4 Hours', dur: 2400, key: 'food'},
+                          {text: 'Check BP every 6 Hours', dur: 3600, key: 'bp'},
+                          {text: 'Drink 8oz Water every 8 Hours', dur: 4800, key: 'water'},
+                          {text: 'Check Pulse O2 every 10 Hours', dur: 6000, key: 'pulseox'},
                       ]}
                           renderItem={({item}) => <Button style={styles.button} 
                               onPress={() => { 
-                                this.AddTime(item.key, item.dur)
-                                Alert.alert(item.key + ' timer is now ' + this.state[item.id]);
+                                this.addTime(item.key, item.dur)
+                                Alert.alert(item.key + ' timer is now ' + item.dur);
                               }} 
-                              title={item.key} />
+                              title={item.text} />
                           }
                       />
                 </View>
                 <View>
                     <FlatList 
                         data={[
-                          {key: this.state.nap},
-                          {key: this.state.food},
-                          {key: this.state.bp},
-                          {key: this.state.water},
-                          {key: this.state.pulseox},
+                          {key: "nap", val: this.state.nap},
+                          {key: "food", val: this.state.food},
+                          {key: "bp", val: this.state.bp},
+                          {key: "water", val: this.state.water},
+                          {key: "pulseox", val: this.state.pulseox},
                         ]}
                         renderItem={({item}) => 
-                            <Text>{item.key} is set to {this.state[item.key]}</Text>
+                            <Text>{item.key} is set to {item.val}</Text>
                         }
                         >
                     </FlatList>
@@ -72,11 +77,25 @@ export default class HomeScreen extends React.Component {
             </View>
         );
     }
-    AddTime = (id, dur) => {  
+ 
+    addTime = (id, dur) => {  
       this.setState({
           [id]: dur
       })
     }
+    timer = () => {
+        this.setState({
+            nap: this.state.nap - 1,
+            food: this.state.food - 1,
+            bp: this.state.bp - 1,
+            water: this.state.water - 1,
+            pulseox: this.state.pulseox - 1,
+        })
+        // Object.keys(this.state.timers).map(
+        //     i => this.setState({ [i]: [i] - 1 })
+        //     )
+    }
+     
 }
 
 const styles = StyleSheet.create({
